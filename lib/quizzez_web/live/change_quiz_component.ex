12a6ef26f1009be5aea03bff50f5f565c95dde5a
Rickard Jonsson <44543626+rickaard@users.noreply.github.com>
2021-12-12 21:38:02 +0100

    Save quiz with user association from form (#11)
    Add the current user as assoc when saving quiz

--- lib/quizzez_web/live/change_quiz_component.ex
+++ lib/quizzez_web/live/change_quiz_component.ex
@@ -5,42 +5,66 @@ defmodule QuizzezWeb.ChangeQuizComponent do
   use QuizzezWeb, :live_view
   use Phoenix.HTML
 
-  # alias Quizzez.Repo
+  alias Quizzez.Repo
+  alias Quizzez.Accounts.User
   alias Quizzez.Quizzes.Quiz
   alias Quizzez.Quizzes.Question
   alias Quizzez.Quizzes.Answer
   alias Quizzez.Quizzes
 
-  def mount(_params, _session, socket) do
-    changeset =
-      Quizzes.change_quiz(%Quiz{
-        questions: [
-          %Question{
-            answers: [
-              %Answer{},
-              %Answer{},
-              %Answer{},
-              %Answer{}
-            ]
-          }
-        ]
-      })
+  def mount(_params, %{"user_id" => user_id} = _session, socket) do
+    socket =
+      socket
+      |> assign(:changeset, empty_full_quiz())
+      |> assign(:user_id, user_id)
 
-    {:ok, assign(socket, %{changeset: changeset})}
+    {:ok, socket}
   end
 
   def handle_event("validate", %{"quiz" => quiz_params}, socket) do
-    IO.inspect(quiz_params, label: "params")
-
     changeset =
       %Quiz{}
       |> Quizzes.change_quiz(quiz_params)
-      |> Map.put(:action, :insert)
+      |> Map.put(:action, :validate)
 
     {:noreply, assign(socket, changeset: changeset)}
   end
 
-  def handle_event("save", _params, socket) do
-    {:noreply, assign(socket, changeset: Quizzes.change_quiz(%Quiz{}))}
+  def handle_event("save", %{"quiz" => quiz} = _params, socket) do
+    user = %User{} = Repo.get(User, socket.assigns.user_id)
+
+    changeset =
+      user
+      |> Ecto.build_assoc(:quizzes)
+      |> Quiz.changeset(quiz)
+
+    case Repo.insert(changeset) do
+      {:ok, %Quiz{}} ->
+        socket =
+          socket
+          |> assign(:changeset, empty_full_quiz())
+          |> put_flash(:info, "Quiz saved 👍")
+          |> redirect(to: Routes.page_path(socket, :index))
+
+        {:noreply, socket}
+
+      {:error, changeset} ->
+        {:noreply, assign(socket, changeset: changeset)}
+    end
+  end
+
+  defp empty_full_quiz do
+    Quizzes.change_quiz(%Quiz{
+      questions: [
+        %Question{
+          answers: [
+            %Answer{},
+            %Answer{},
+            %Answer{},
+            %Answer{}
+          ]
+        }
+      ]
+    })
   end
 end
