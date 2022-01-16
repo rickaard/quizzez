b4da5b249ee44a68aece979bc04f3cf54a26dbf0
Rickard <rickaard@gmail.com>
2022-01-16 19:30:10 +0100

    Add tests for profile controller

--- test/support/factories.ex
+++ test/support/factories.ex
@@ -2,12 +2,26 @@ defmodule QuizzezWeb.Factories do
   use ExMachina.Ecto, repo: Quizzez.Repo
 
   alias Quizzez.Accounts
+  alias Quizzez.Quizzes
 
   def user_factory do
     %Accounts.User{
       email: sequence(:email, &"email-#{&1}@example.com"),
       provider: "google",
       name: "John Doe"
+    }
+  end
+
+  def quiz_factory do
+    %Quizzes.Quiz{
+      title: sequence(:title, &"Quiz-#{&1} title"),
+      description: "Quiz description"
+    }
+  end
+
+  def question_factory do
+    %Quizzes.Question{
+      text: sequence(:title, &"What is question #{&1}?")
     }
   end
 end
