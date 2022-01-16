0f87a4df052491f2aac7b2d2488f68c3f1ebfac3
Rickard <rickaard@gmail.com>
2022-01-16 19:28:31 +0100

    Add function to pluralize quizzes text

--- lib/quizzez_web/views/profile_view.ex
+++ lib/quizzez_web/views/profile_view.ex
@@ -1,3 +1,6 @@
 defmodule QuizzezWeb.ProfileView do
   use QuizzezWeb, :view
+
+  def quiz_plural_text(quiz_list) when length(quiz_list) == 1, do: "quiz"
+  def quiz_plural_text(_quiz_list), do: "quizzes"
 end
