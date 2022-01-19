bf5c5613776e9a970d92c92d02c29f14e5089b25
Rickard <rickaard@gmail.com>
2022-01-19 22:43:02 +0100

    Add `categories` as new DB table

---
+++ lib/quizzez/quizzes/categories.ex
@@ -0,0 +1,18 @@
+defmodule Quizzez.Quizzes.Categories do
+  use Quizzez.Schema
+  import Ecto.Changeset
+
+  schema "categories" do
+    field :name, :string
+
+    has_many :quizzes, Quizzez.Quizzes.Quiz
+
+    timestamps()
+  end
+
+  def changeset(changeset, params) do
+    changeset
+    |> cast(params, [:name])
+    |> validate_required([:name])
+  end
+end
