f53f9f14f494165f8b83697081264ff47cb56f8f
Rickard <rickaard@gmail.com>
2022-01-29 00:42:05 +0100

    Add amount of questions in quiz detail view

--- test/quizzez_web/controllers/quiz/participation_controller_test.exs
+++ test/quizzez_web/controllers/quiz/participation_controller_test.exs
@@ -11,11 +11,16 @@ defmodule QuizzezWeb.Survey.ParticipationControllerTest do
           user: build(:user)
         )
 
+      insert(:question, quiz: quiz, text: "This is the first question")
+      insert(:question, quiz: quiz, text: "This is the second question")
+      insert(:question, quiz: quiz, text: "This is the third question")
+
       conn = get(conn, Routes.quiz_participation_path(conn, :show, quiz.id))
 
       response = html_response(conn, 200)
       assert response =~ "A simple quiz"
       assert response =~ "A quiz description"
+      assert response =~ "3 questions"
     end
   end
 end
