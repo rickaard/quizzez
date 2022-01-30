708609b84ba990bc50fb4918fedd7bbc91ba34a0
Rickard <rickaard@gmail.com>
2022-01-30 23:58:02 +0100

    Add logic to start quiz

--- lib/quizzez_web/live/quiz_participation_component.ex
+++ lib/quizzez_web/live/quiz_participation_component.ex
@@ -9,13 +9,45 @@ defmodule QuizzezWeb.QuizParticipationComponent do
     socket =
       socket
       |> assign(:quiz, quiz)
-      |> assign(:current_question, 0)
+      |> assign(:current_question, nil)
+      |> assign(:current_question_index, nil)
+      |> assign(:status, :unstarted)
 
     {:ok, socket}
   end
 
   def handle_event("next_question", _params, socket) do
-    {:noreply, socket}
+    all_questions = socket.assigns.quiz.questions
+
+    case socket.assigns.status do
+      :unstarted ->
+        first_question = List.first(all_questions)
+
+        socket =
+          socket
+          |> assign(:current_question, first_question)
+          |> assign(:current_question_index, 0)
+          |> assign(:status, :started)
+
+        {:noreply, socket}
+
+      :started ->
+        prev_question_index = socket.assigns.current_question_index
+
+        next_question =
+          Enum.at(
+            all_questions,
+            prev_question_index + 1,
+            socket.assigns.current_question
+          )
+
+        socket =
+          socket
+          |> assign(:current_question, next_question)
+          |> assign(:current_question_index, prev_question_index + 1)
+
+        {:noreply, assign(socket, :current_question, next_question)}
+    end
   end
 
   defp question_amount_text(questions) when length(questions) == 1, do: "1 question"
--- test/quizzez_web/live/quiz_participation_component_test.exs
+++ test/quizzez_web/live/quiz_participation_component_test.exs
@@ -27,5 +27,32 @@ defmodule QuizzezWeb.QuizParticipationComponentTest do
       assert html =~ "A quiz description"
       assert html =~ "3 questions"
     end
+
+    test "shows first question when starting quiz", %{conn: conn} do
+      question_1 =
+        insert(:question,
+          text: "This is the first question",
+          answers: [
+            build(:answer, is_correct: true, text: "This is first answer"),
+            build(:answer, is_correct: false, text: "This is second answer")
+          ]
+        )
+
+      quiz =
+        insert(
+          :quiz,
+          title: "A simple quiz",
+          description: "A quiz description",
+          user: build(:user),
+          questions: [question_1]
+        )
+
+      {:ok, view, _html} =
+        live_isolated(conn, QuizParticipationComponent, session: %{"quiz" => quiz})
+
+      assert view
+             |> element("button", "Start quiz")
+             |> render_click() =~ "This is the first question"
+    end
   end
 end
