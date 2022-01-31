33ee97d900fc6f16dd858aec6801ced604e801d1
Rickard <rickaard@gmail.com>
2022-01-31 22:58:01 +0100

    Add tests for previous and next button

--- lib/quizzez_web/live/quiz_participation_component.ex
+++ lib/quizzez_web/live/quiz_participation_component.ex
@@ -12,44 +12,74 @@ defmodule QuizzezWeb.QuizParticipationComponent do
       |> assign(:current_question, nil)
       |> assign(:current_question_index, nil)
       |> assign(:status, :unstarted)
+      |> assign(:questions_amount, length(quiz.questions))
 
     {:ok, socket}
   end
 
-  def handle_event("next_question", _params, socket) do
+  def handle_event("start-quiz", _params, socket) do
     all_questions = socket.assigns.quiz.questions
+    first_question = List.first(all_questions)
 
-    case socket.assigns.status do
-      :unstarted ->
-        first_question = List.first(all_questions)
+    socket =
+      socket
+      |> assign(:current_question, first_question)
+      |> assign(:current_question_index, 0)
+      |> assign(:status, :started)
 
-        socket =
-          socket
-          |> assign(:current_question, first_question)
-          |> assign(:current_question_index, 0)
-          |> assign(:status, :started)
+    {:noreply, socket}
+  end
 
-        {:noreply, socket}
+  def handle_event("prev-question", _params, socket) do
+    all_questions = socket.assigns.quiz.questions
+    prev_question_index = socket.assigns.current_question_index
 
-      :started ->
-        prev_question_index = socket.assigns.current_question_index
+    next_question =
+      Enum.at(
+        all_questions,
+        prev_question_index - 1,
+        socket.assigns.current_question
+      )
 
-        next_question =
-          Enum.at(
-            all_questions,
-            prev_question_index + 1,
-            socket.assigns.current_question
-          )
+    socket =
+      socket
+      |> assign(:current_question, next_question)
+      |> assign(:current_question_index, prev_question_index - 1)
 
-        socket =
-          socket
-          |> assign(:current_question, next_question)
-          |> assign(:current_question_index, prev_question_index + 1)
+    {:noreply, assign(socket, :current_question, next_question)}
+  end
 
-        {:noreply, assign(socket, :current_question, next_question)}
-    end
+  def handle_event("calculate-score", _params, socket) do
+    {:noreply, socket}
+  end
+
+  def handle_event("next-question", _params, socket) do
+    all_questions = socket.assigns.quiz.questions
+    prev_question_index = socket.assigns.current_question_index
+
+    next_question =
+      Enum.at(
+        all_questions,
+        prev_question_index + 1,
+        socket.assigns.current_question
+      )
+
+    socket =
+      socket
+      |> assign(:current_question, next_question)
+      |> assign(:current_question_index, prev_question_index + 1)
+
+    {:noreply, assign(socket, :current_question, next_question)}
   end
 
   defp question_amount_text(questions) when length(questions) == 1, do: "1 question"
   defp question_amount_text(questions), do: "#{length(questions)} questions"
+
+  def last_question?(%{current_question_index: current, questions_amount: questions} = _assigns) do
+    current + 1 == questions
+  end
+
+  def first_question?(%{current_question_index: current} = _assigns) do
+    current == 0
+  end
 end
--- test/quizzez_web/live/quiz_participation_component_test.exs
+++ test/quizzez_web/live/quiz_participation_component_test.exs
@@ -38,6 +38,50 @@ defmodule QuizzezWeb.QuizParticipationComponentTest do
           ]
         )
 
+      question_2 =
+        insert(:question,
+          text: "This is the second question",
+          answers: [
+            build(:answer, is_correct: true, text: "This is first answer"),
+            build(:answer, is_correct: false, text: "This is second answer")
+          ]
+        )
+
+      quiz =
+        insert(
+          :quiz,
+          title: "A simple quiz",
+          description: "A quiz description",
+          user: build(:user),
+          questions: [question_1, question_2]
+        )
+
+      {:ok, view, _html} =
+        live_isolated(conn, QuizParticipationComponent, session: %{"quiz" => quiz})
+
+      # assert view
+      #        |> element("button", "Start quiz")
+      #        |> render_click() =~ "This is the first question"
+      html =
+        view
+        |> element("button", "Start quiz")
+        |> render_click()
+
+      refute html =~ "Start quiz"
+      assert html =~ "This is the first question"
+      assert html =~ "Next question"
+    end
+
+    test "shows `Show score` when no questions left to answer", %{conn: conn} do
+      question_1 =
+        insert(:question,
+          text: "This is the first question",
+          answers: [
+            build(:answer, is_correct: true, text: "This is first answer"),
+            build(:answer, is_correct: false, text: "This is second answer")
+          ]
+        )
+
       quiz =
         insert(
           :quiz,
@@ -50,9 +94,96 @@ defmodule QuizzezWeb.QuizParticipationComponentTest do
       {:ok, view, _html} =
         live_isolated(conn, QuizParticipationComponent, session: %{"quiz" => quiz})
 
-      assert view
-             |> element("button", "Start quiz")
-             |> render_click() =~ "This is the first question"
+      html =
+        view
+        |> element("button", "Start quiz")
+        |> render_click()
+
+      refute html =~ "Next question"
+      assert html =~ "Show score"
+    end
+
+    test "does not show `Previous question` button when on first question", %{conn: conn} do
+      question_1 =
+        insert(:question,
+          text: "This is the first question",
+          answers: [
+            build(:answer, is_correct: true, text: "This is first answer"),
+            build(:answer, is_correct: false, text: "This is second answer")
+          ]
+        )
+
+      question_2 =
+        insert(:question,
+          text: "This is the second question",
+          answers: [
+            build(:answer, is_correct: true, text: "This is first answer"),
+            build(:answer, is_correct: false, text: "This is second answer")
+          ]
+        )
+
+      quiz =
+        insert(
+          :quiz,
+          title: "A simple quiz",
+          description: "A quiz description",
+          user: build(:user),
+          questions: [question_1, question_2]
+        )
+
+      {:ok, view, _html} =
+        live_isolated(conn, QuizParticipationComponent, session: %{"quiz" => quiz})
+
+      html =
+        view
+        |> element("button", "Start quiz")
+        |> render_click()
+
+      assert html =~ "Next question"
+      refute html =~ "Previous question"
+    end
+
+    test "do show `Previous question` when not on first question", %{conn: conn} do
+      question_1 =
+        insert(:question,
+          text: "This is the first question",
+          answers: [
+            build(:answer, is_correct: true, text: "This is first answer"),
+            build(:answer, is_correct: false, text: "This is second answer")
+          ]
+        )
+
+      question_2 =
+        insert(:question,
+          text: "This is the second question",
+          answers: [
+            build(:answer, is_correct: true, text: "This is first answer"),
+            build(:answer, is_correct: false, text: "This is second answer")
+          ]
+        )
+
+      quiz =
+        insert(
+          :quiz,
+          title: "A simple quiz",
+          description: "A quiz description",
+          user: build(:user),
+          questions: [question_1, question_2]
+        )
+
+      {:ok, view, _html} =
+        live_isolated(conn, QuizParticipationComponent, session: %{"quiz" => quiz})
+
+      view
+      |> element("button", "Start quiz")
+      |> render_click()
+
+      html =
+        view
+        |> element("button", "Next question")
+        |> render_click()
+
+      assert html =~ "Previous question"
     end
   end
 end
