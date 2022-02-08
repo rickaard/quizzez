5ac48f5751f0b5f95c2f0dafe6add31cdc2038cc
Rickard <rickaard@gmail.com>
2022-02-08 23:29:15 +0100

    Add test for empty quiz list

--- test/quizzez_web/controllers/category_controller_test.exs
+++ test/quizzez_web/controllers/category_controller_test.exs
@@ -17,4 +17,28 @@ defmodule QuizzezWeb.CategoryControllerTest do
       assert response =~ "Workout"
     end
   end
+
+  describe "SHOW /" do
+    test "renders only quizzes for specific category", %{conn: conn} do
+      user = insert(:user)
+      insert(:quiz, user: user, category: "misc")
+      insert(:quiz, user: user, category: "programming")
+
+      conn = get(conn, Routes.category_path(conn, :show, "misc"))
+
+      response = html_response(conn, 200)
+      assert response =~ "misc"
+      refute response =~ "programming"
+    end
+
+    test "renders `no quizzes created` text", %{conn: conn} do
+      insert(:quiz, user: build(:user), category: "programming")
+
+      conn = get(conn, Routes.category_path(conn, :show, "misc"))
+
+      response = html_response(conn, 200)
+      assert response =~ "No quizzes created for this category yet"
+      refute response =~ "misc"
+    end
+  end
 end
