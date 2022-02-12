27c6693722e23d397a7482b89e1ddc60c3b794ad
Rickard Jonsson <44543626+rickaard@users.noreply.github.com>
2022-02-12 02:50:15 +0100

    Add user menu item in dropdown (#39)
    This change will add menu items for logged in user (for now just link to profile and log out link) to a dropdown instead of rendering them directly in the header.

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,26 +1,7 @@
-// We import the CSS which is extracted to its own file by esbuild.
-// Remove this line if you add a your own CSS build pipeline (e.g postcss).
 import "../css/app.css";
-
-// If you want to use Phoenix channels, run `mix help phx.gen.channel`
-// to get started and then uncomment the line below.
-// import "./user_socket.js"
-
-// You can include dependencies in two ways.
-//
-// The simplest option is to put them in assets/vendor and
-// import them using relative paths:
-//
-//     import "./vendor/some-package.js"
-//
-// Alternatively, you can `npm install some-package` and import
-// them using a path starting with the package name:
-//
-//     import "some-package"
-//
-
-// Include phoenix_html to handle method=PUT/DELETE in forms and buttons.
 import "phoenix_html";
+import { handleMenuClick } from "./utils/user-menu";
+
 // Establish Phoenix Socket and LiveView configuration.
 import { Socket } from "phoenix";
 import { LiveSocket } from "phoenix_live_view";
@@ -41,8 +22,5 @@ window.addEventListener("phx:page-loading-stop", (info) => topbar.hide());
 // connect if there are any LiveViews on the page
 liveSocket.connect();
 
-// expose liveSocket on window for web console debug logs and latency simulation:
-// >> liveSocket.enableDebug()
-// >> liveSocket.enableLatencySim(1000)  // enabled for duration of browser session
-// >> liveSocket.disableLatencySim()
 window.liveSocket = liveSocket;
+handleMenuClick();
--- lib/quizzez_web/views/layout_view.ex
+++ lib/quizzez_web/views/layout_view.ex
@@ -2,8 +2,16 @@ defmodule QuizzezWeb.LayoutView do
   use QuizzezWeb, :view
 
   alias QuizzezWeb.SVGHelpers
+  alias QuizzezWeb.Authentication
+  alias Quizzez.Accounts.User
 
   # Phoenix LiveDashboard is available only in development by default,
   # so we instruct Elixir to not warn if the dashboard route is missing.
   @compile {:no_warn_undefined, {Routes, :live_dashboard_path, 2}}
+
+  def user_name(conn) do
+    %User{name: name} = Authentication.get_current_user(conn)
+
+    name
+  end
 end
