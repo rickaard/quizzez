2aecb6ca20cd916cf2a2b02b8f22e60d4cf986d5
Rickard <rickaard@gmail.com>
2022-02-20 10:18:52 +0100

    Upgrade Pheonix version

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Quizzez.MixProject do
     [
       app: :quizzez,
       version: "0.1.0",
-      elixir: "~> 1.12",
+      elixir: "~> 1.13",
       elixirc_paths: elixirc_paths(Mix.env()),
       compilers: [:gettext] ++ Mix.compilers(),
       start_permanent: Mix.env() == :prod,
@@ -34,7 +34,7 @@ defmodule Quizzez.MixProject do
   # Type `mix help deps` for examples and options.
   defp deps do
     [
-      {:phoenix, "~> 1.6.2"},
+      {:phoenix, "~> 1.6.3"},
       {:phoenix_ecto, "~> 4.4"},
       {:ecto_sql, "~> 3.6"},
       {:postgrex, ">= 0.0.0"},
