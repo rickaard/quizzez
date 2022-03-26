71d02182ad0d3de01749044f5714ede402ae94f7
Rickard <rickaard@gmail.com>
2022-03-26 01:40:54 +0100

    Fix failing tests

--- test/quizzez_web/live/quiz_participation_component_test.exs
+++ test/quizzez_web/live/quiz_participation_component_test.exs
@@ -66,10 +66,10 @@ defmodule QuizzezWeb.QuizParticipationComponentTest do
 
       refute html =~ "Start quiz"
       assert html =~ "This is the first question"
-      assert html =~ "Next question"
+      assert html =~ "Next"
     end
 
-    test "shows `Show score` when no questions left to answer", %{conn: conn} do
+    test "shows `Score` when no questions left to answer", %{conn: conn} do
       question_1 =
         insert(:question,
           text: "This is the first question",
@@ -97,10 +97,10 @@ defmodule QuizzezWeb.QuizParticipationComponentTest do
         |> render_click()
 
       refute html =~ "Next question"
-      assert html =~ "Show score"
+      assert html =~ "Score"
     end
 
-    test "does not show `Previous question` button when on first question", %{conn: conn} do
+    test "does not show `Previous` button when on first question", %{conn: conn} do
       question_1 =
         insert(:question,
           text: "This is the first question",
@@ -136,11 +136,11 @@ defmodule QuizzezWeb.QuizParticipationComponentTest do
         |> element("button", "Start quiz")
         |> render_click()
 
-      assert html =~ "Next question"
-      refute html =~ "Previous question"
+      assert html =~ "Next"
+      refute html =~ "Previous"
     end
 
-    test "do show `Previous question` when not on first question", %{conn: conn} do
+    test "do show `Previous` when not on first question", %{conn: conn} do
       question_1 =
         insert(:question,
           text: "This is the first question",
@@ -181,43 +181,10 @@ defmodule QuizzezWeb.QuizParticipationComponentTest do
 
       html =
         view
-        |> element("button", "Next question")
+        |> element("button", "Next")
         |> render_click()
 
-      assert html =~ "Previous question"
+      assert html =~ "Previous"
     end
-
-    # test "clicked answer", %{conn: conn} do
-    #   question_1 =
-    #     insert(:question,
-    #       text: "This is the first question",
-    #       answers: [
-    #         build(:answer, is_correct: true, text: "This is first answer"),
-    #         build(:answer, is_correct: false, text: "This is second answer")
-    #       ]
-    #     )
-
-    #   quiz =
-    #     insert(
-    #       :quiz,
-    #       title: "A simple quiz",
-    #       description: "A quiz description",
-    #       user: build(:user),
-    #       questions: [question_1]
-    #     )
-
-    #   {:ok, view, _html} =
-    #     live_isolated(conn, QuizParticipationComponent, session: %{"quiz" => quiz})
-
-    #   view
-    #   |> element("button", "Start quiz")
-    #   |> render_click()
-
-    #   view
-    #   |> element("button", "Next question")
-    #   |> render_click()
-
-    #   assert html =~ "Previous question"
-    # end
   end
 end
