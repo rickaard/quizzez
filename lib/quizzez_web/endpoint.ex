8bfc833b1b3f2b32a49b638b0e29642db3a407cf
Rickard <rickaard@gmail.com>
2022-10-21 00:18:32 +0200

    Allow `svg` folder to be served

--- lib/quizzez_web/endpoint.ex
+++ lib/quizzez_web/endpoint.ex
@@ -20,7 +20,7 @@ defmodule QuizzezWeb.Endpoint do
     at: "/",
     from: :quizzez,
     gzip: false,
-    only: ~w(assets fonts images favicon.ico robots.txt)
+    only: ~w(assets fonts images svg illustrations favicon.ico robots.txt)
 
   # Code reloading can be explicitly enabled under the
   # :code_reloader configuration of your endpoint.
